3f57c7a6e92fdaa6ce7e1d244fc769d411189d8b
Anabella Freites <afreites.25@est.ucab.edu.ve>
2025-05-24 17:09:46 -0400

    creacion de funciones para crear el mazo y repartirlo

--- rondas.h
+++ rondas.h
@@ -16,13 +16,14 @@ struct jugador {
 };
 
 //variables
-cartas mazo[52]; // numero total de cartas
+cartas mazo[54]; // numero total de cartas
 jugador jugadores[4]; //maximo de jugadores
 int jugadorActual = 0; //indice del jugador actual (para saber de quien es el turno)
+int njugadores = 4; //numero total de jugadores
 
 //funcion para intercambiar las cartas (se usara para ordenarlas)
 void intercambiar (cartas *a, cartas *b){
-    cartas aux = *a;
+    cartas aux = *a; //auxiliar para no perder el valor de la primera carta
     *a = *b;
     *b = aux;
 
@@ -31,14 +32,49 @@ void intercambiar (cartas *a, cartas *b){
 //funcion para ordenar las cartas en las manos de los jugadores
 void ordenar_cartas (jugador *p){
     int i = 0;
-    while (i < p -> tamano_mano -1){
-        int j = i + 1;
-        while (j < p -> tamano_mano){
-            if (p -> mano[i].rango_cartas > p -> mano[j].rango_cartas){
+    while (i < p -> tamano_mano -1){ //indice menor a lo que esta apuntando p menos 1
+        int j = i + 1; //indice auxiliar
+        while (j < p -> tamano_mano){ //indice menor a lo que esta apuntando p
+            if (p -> mano[i].rango_cartas > p -> mano[j].rango_cartas){ //comparacion de dos cartas dentro del arreglo
                 intercambiar(&p -> mano[i], &p -> mano[j]);
             }
             j ++;
         }
         i ++;
+    }
+}
+
+//funcion para inicializar un mazo
+void crear_mazo(){
+    int i = 0; //indice general
+    int p = 0; //indice de los palos de las cartas (0 = trebol, 1 = diamante, 2 = corazon, 3 = espada)
+    while (p < 4){
+        int r = 0; //indice del rango de las cartas (del 0 al 12)
+        while (r < 13){
+            mazo[i].rango_cartas = r; //asignacion del rango de la carta
+            mazo[i].palo = p; //asignacion del palo de la carta
+            i ++;
+            r ++;
+        }
+        p ++;
+    }
+}
+
+//funcion para repartir cartas
+void repartir(){
+    int i = 0; //indice general
+    while (i < njugadores){ //reiniciar el tamano de las manos para no usar cartas de rondas anteriores
+        jugadores[i].tamano_mano = 0;
+        i ++;
+    }
+
+    int j = 0; //indice auxiliar
+    int p = 0; //indice del jugador actual
+    while (j < 54){
+        jugadores[p].mano[jugadores[p].tamano_mano] = mazo[j]; //darle na carta a un jugador
+        jugadores[p].tamano_mano ++; //aumentar el tamano de la mano de dicho jugador
+        j ++;
+        p ++;
+        if (p >= njugadores) p = 0; //volver al primer jugador despues de repartir
     }
 }
