a1e6dacd6420c12a0749477eb16ef43cbaa67be2
Anabella Freites <afreites.25@est.ucab.edu.ve>
2025-05-24 21:38:59 -0400

    se cortaron algunas funciones para moverlas al archivo rondas.h

--- primitivas_pilas.h
+++ primitivas_pilas.h
@@ -7,23 +7,7 @@ struct Node {
     Node *next;
 };
 
-void push(listCartas *p, int rangoCartas, int paloCartas) {
-    NodeCartas *newElement = new NodeCartas;
-    newElement->rangoCartas = rangoCartas;
-    newElement->paloCartas = paloCartas;
-    newElement->next = p->head;
-    p->head = newElement;
-    p->size++;
-}
 
-NodeCartas* pop(listCartas *p) {
-    if (!p || !(p->head)) return NULL;
-    NodeCartas *aux = p->head;
-    p->head = p->head->next;
-    p->size--;
-    aux->next = NULL;
-    return aux;
-}
 
 int top(Node *p) {
     if (!p) return 0;
