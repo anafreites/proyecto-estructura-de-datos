3f00b37be3c726a5fa2f2d55c1b43cba7c8cea7d
Anabella Freites <afreites.25@est.ucab.edu.ve>
2025-05-26 23:34:47 -0400

    impotacion de la libreria cartas

--- Proyecto1_JuegoCartas.cpp
+++ Proyecto1_JuegoCartas.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include "menu.h"
 #include "rondas.h"
+#include "cartas.h"
 
 using namespace std;
 
 //variables globales
 int ronda = 1;
+int turno;
 
 int main(){
     obtenerOpcionMenu(1, 3);
-    while (ronda <= 3){
-        cout << "Ronda " << ronda << endl;
-        initRonda();
+    while (ronda < 4){
+        cout << "Ronda #" << ronda << endl;
+        initRonda(ronda);
         ronda ++;
     }
     return 0;
